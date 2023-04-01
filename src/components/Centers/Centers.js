83cb0fa625e47bda5b92264578ffc7663ba1bc75
naji-alhusami <najihussami@gmail.com>
2023-04-02 01:01:44 +0300

    added checkbox for each location

--- src/components/Centers/Centers.js
+++ src/components/Centers/Centers.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
@@ -17,9 +17,18 @@ import markerIcon2x from "../Images/marker-icon-2x.png";
 
 const Centers = () => {
   const dispatch = useDispatch();
+
   const { center, loading } = useSelector((state) => state.center);
-  console.log(center);
   const healthCenters = center;
+
+  const [isListed, setIsListed] = useState(healthCenters.map(() => true));
+  
+  const handleCheckboxChange = (event, index) => {
+    const newList = [...isListed];
+    newList[index] = event.target.checked;
+    setIsListed(newList);
+    console.log(isListed);
+  };
 
   useEffect(() => {
     const getData = () => {
@@ -118,6 +127,9 @@ const Centers = () => {
               <th className="w-[10rem] border border-gray-400 px-4 py-2">
                 Delete
               </th>
+              <th className="w-[10rem] border border-gray-400 px-4 py-2">
+                List
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -128,13 +140,6 @@ const Centers = () => {
                     type="text"
                     className="w-full"
                     value={healthCenter.name}
-                    // onChange={(e) =>
-                    //   setHealthCenters([
-                    //     ...healthCenters.slice(0, index),
-                    //     { ...healthCenter, name: e.target.value },
-                    //     ...healthCenters.slice(index + 1),
-                    //   ])
-                    // }
                     readOnly
                   />
                 </td>
@@ -143,28 +148,14 @@ const Centers = () => {
                     type="text"
                     className="w-full"
                     value={healthCenter.address}
-                    // onChange={(e) =>
-                    //   setHealthCenters([
-                    //     ...healthCenters.slice(0, index),
-                    //     { ...healthCenter, address: e.target.value },
-                    //     ...healthCenters.slice(index + 1),
-                    //   ])
-                    // }
                     readOnly
                   />
                 </td>
-                <td className="border border-gray-400 px-4 py-2 ">
+                <td className="border border-gray-400 text-center px-4 py-2 ">
                   <input
                     type="text"
                     className="w-full"
                     value={healthCenter.category}
-                    // onChange={(e) =>
-                    //   setHealthCenters([
-                    //     ...healthCenters.slice(0, index),
-                    //     { ...healthCenter, category: e.target.value },
-                    //     ...healthCenters.slice(index + 1),
-                    //   ])
-                    // }
                     readOnly
                   />
                 </td>
@@ -177,6 +168,14 @@ const Centers = () => {
                     Remove
                   </button>
                 </td>
+                <td className="border border-gray-400 text-center px-4 py-2 ">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox h-5 w-5 px-4 py-2 text-gray-600"
+                    checked={isListed[index]}
+                    onChange={(event) => handleCheckboxChange(event, index)}
+                  />
+                </td>
               </tr>
             ))}
           </tbody>
@@ -187,34 +186,36 @@ const Centers = () => {
             className="justify-center "
             center={[51.505, -0.09]}
             zoom={13}
-            // scrollWheelZoom={true}
           >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {healthCenters.map((healthCenter, index) => (
-              <Marker
-                // key={index}
-                position={[healthCenter.lat, healthCenter.lon]}
-                icon={markerIcon}
-              >
-                <Popup>
-                  <div>
-                    <h3>Name: {healthCenter.name}</h3>
-                    <p>Adress: {healthCenter.address}</p>
-                    <p>Category: {healthCenter.category}</p>
-                    <button
-                      className="bg-cyan-300 p-2 rounded-md"
-                      type="button"
-                      onClick={() => deleteHealthCenter(index)}
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </Popup>
-              </Marker>
-            ))}
+            {healthCenters.map(
+              (healthCenter, index) =>
+                isListed[index] && (
+                  <Marker
+                    // key={index}
+                    position={[healthCenter.lat, healthCenter.lon]}
+                    icon={markerIcon}
+                  >
+                    <Popup>
+                      <div>
+                        <h3>Name: {healthCenter.name}</h3>
+                        <p>Adress: {healthCenter.address}</p>
+                        <p>Category: {healthCenter.category}</p>
+                        <button
+                          className="bg-cyan-300 p-2 rounded-md"
+                          type="button"
+                          onClick={() => deleteHealthCenter(index)}
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </Popup>
+                  </Marker>
+                )
+            )}
           </MapContainer>
         </div>
       </div>
