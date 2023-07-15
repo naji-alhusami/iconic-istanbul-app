2dc38d542a955959bef0d3b495f92facaa2b6c49
naji-alhusami <najihussami@gmail.com>
2023-07-15 17:51:27 +0300

    edit Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,30 +11,30 @@ const Home = () => {
       className=" w-screen h-screen bg-cover bg-top bg-no-repeat"
       style={{ backgroundImage: `url(${BackgroundImage})`, top: 0 }}
     >
-      <div className="flex flex-col pt-12 pb-12 mr-6  items-end md:flex-col   lg:flex-row lg:justify-end md:items-end md:mr-12 sm:mr-12 lg:pt-30 xl:mr-12 lg:pb-20 md:pt-20">
-        <div className=" flex flex-col items-end   md:flex-col md:items-  lg:items-center lg:mr-12">
+      <div className="flex flex-col pt-12 pb-12  items-end md:flex-col   lg:flex-row lg:justify-end md:items-end lg:pt-30 lg:pb-20 md:pt-20">
+        <div className="bg-opacity-90 bg-orange-800 rounded-l-md flex flex-col items-center p-10  md:flex-col md:items-center  lg:items-center">
           <motion.p
-            initial={{ x: 100, opacity: 0 }}
+            initial={{ x: 80, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
-            className="text-3xl mb-5 md:text-4xl lg:text-5xl text-bold"
+            className="text-3xl mb-5 md:text-4xl lg:text-5xl text-white"
           >
             Please Check
           </motion.p>
           <motion.p
-            initial={{ x: -100, opacity: 0 }}
+            initial={{ x: -40, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
-            className="text-3xl mb-5 md:text-4xl lg:text-5xl text-bold"
+            className="text-3xl mb-5 md:text-4xl lg:text-5xl text-white"
           >
-            Our Health Centers
+            Iconic Places In Istanbul
           </motion.p>
           <Link to="/healthcenters">
             <button
               type="button"
-              className="text-md mb-8 rounded-md box-border p-2 transition-all duration-250 bg-cyan-400 hover:bg-cyan-500 hover:text-white md:text-2xl"
+              className="text-md mb-8 rounded-md box-border p-2 transition-all duration-250 text-white font-bold bg-cyan-400 hover:bg-cyan-500 hover:text-white md:text-2xl"
             >
-              Health Centers
+              ICONIC PLACES
             </button>
           </Link>
         </div>
