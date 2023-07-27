bfdd188490d6941a018cd2ad10122716b05b445a
naji-alhusami <najihussami@gmail.com>
2023-07-27 16:24:26 +0300

    slidesData have been added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,38 +3,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+import { SlidesData } from "./SlidesData";
 import "./Home.css";
 
 const Home = () => {
+  // const [currentSlide, setCurrentSlide] = useState(0);
+
   return (
-    <div className="slider w-screen h-screen flex flex-col items-center justify-center bg-cover bg-top bg-no-repeat">
-      <div className=" justify-center flex flex-col items-center md:flex-col lg:flex-row lg:justify-end md:items-end lg:pt-30 lg:pb-20 md:pt-20">
-        <div className=" bg-opacity-90 bg-orange-800 rounded-md flex flex-col items-center p-2 md:p-8  md:flex-col md:items-center  lg:items-center">
-          <motion.p
-            initial={{ x: 80, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1 }}
-            className="text-3xl mb-5 md:text-4xl lg:text-5xl text-white"
-          >
-            Please Check
-          </motion.p>
-          <motion.p
-            initial={{ x: -40, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1 }}
-            className="text-3xl mb-8 md:text-4xl lg:text-5xl text-white"
-          >
-            Iconic Places In Istanbul
-          </motion.p>
-          <Link to="/iconicplaces">
-            <button
-              type="button"
-              className="text-md mb-8 rounded-md box-border p-2 transition-all duration-250 text-white font-bold bg-cyan-400 hover:bg-cyan-500 hover:text-white md:text-2xl"
-            >
-              ICONIC PLACES
-            </button>
-          </Link>
-        </div>
+    <div className=" w-screen h-screen flex flex-col items-center justify-center bg-cover bg-top bg-no-repeat">
+      <div className="slider justify-center flex flex-col items-center md:flex-col lg:flex-row lg:justify-end md:items-end lg:pt-30 lg:pb-20 md:pt-20">
+        {SlidesData.map((data) => {
+          return (
+            <div className=" bg-opacity-90 bg-orange-800 rounded-md flex flex-col items-center p-2 md:p-8  md:flex-col md:items-center  lg:items-center">
+              <motion.p
+                initial={{ x: 80, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1 }}
+                className="text-3xl mb-5 md:text-4xl lg:text-5xl text-white"
+              >
+                {data.heading}
+              </motion.p>
+              <motion.p
+                initial={{ x: -40, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1 }}
+                className="text-3xl mb-8 md:text-4xl lg:text-5xl text-white"
+              >
+                {data.subheading}
+              </motion.p>
+              <Link to="/iconicplaces">
+                <button
+                  type="button"
+                  className="text-md mb-8 rounded-md box-border p-2 transition-all duration-250 text-white font-bold bg-cyan-400 hover:bg-cyan-500 hover:text-white md:text-2xl"
+                >
+                  ICONIC PLACES
+                </button>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/components/Home/SlidesData.js
@@ -0,0 +1,46 @@
+import bg1 from "../Images/bg1.jpg";
+import bg2 from "../Images/bg2.jpg";
+import bg3 from "../Images/bg3.jpg";
+import bg4 from "../Images/bg4.jpg";
+import bg5 from "../Images/bg5.jpg";
+import bg6 from "../Images/bg6.jpg";
+
+export const SlidesData = [
+  {
+    backgound: bg1,
+    heading: "Start your journey of discovery in Istanbul,",
+    subheading: "where others have ventured before you.",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    backgound: bg2,
+    heading: "Escape to Istanbul,",
+    subheading: "your dream destination brought to life.",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    backgound: bg3,
+    heading: "Help others uncover new places in Istanbul,",
+    subheading: "making their exploration more remarkable.",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    backgound: bg4,
+    heading: "Unearth the captivating sights of Istanbul,",
+    subheading: "cherished by all who venture there.",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    backgound: bg5,
+    heading: "Discover unique views in Istanbul,",
+    subheading:
+      "where the captivating combination of Eastern and Western cultures awaits.",
+    buttonText: "ICONIC PLACES",
+  },
+  {
+    backgound: bg6,
+    heading: "Remember to capture the mesmerizing moments,",
+    subheading: "and share them on our website.",
+    buttonText: "ICONIC PLACES",
+  },
+];
