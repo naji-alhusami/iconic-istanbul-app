432808e499572ed76d100fa8c7a8028f8a9b2df3
naji-alhusami <najihussami@gmail.com>
2023-08-03 15:58:00 +0300

    started with about page

--- src/components/AboutContent/AboutContent.js
+++ src/components/AboutContent/AboutContent.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import kulesi from "../Images/kizkulesi.jpg";
+
 const AboutContent = () => {
   return (
-    <div>
-      <h1>naji</h1>
+    <div className="flex flex-col items-center justify-center">
+      <img src={kulesi} alt="kizkulesi" />
+      <h1 className="my-10 text-5xl font-bold text-orange-900 border-amber-900 border-b-4 pb-5">About</h1>
     </div>
   );
 };
