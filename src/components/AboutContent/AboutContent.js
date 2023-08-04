88387119220aec2c55193d417f0e2cbf16557c3b
naji-alhusami <najihussami@gmail.com>
2023-08-04 16:10:24 +0300

    finished About page

--- src/components/AboutContent/AboutContent.js
+++ src/components/AboutContent/AboutContent.js
@@ -1,12 +1,35 @@
 import React from "react";
 
+import { AboutData } from "./AboutData";
 import kulesi from "../Images/kizkulesi.jpg";
 
 const AboutContent = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <img src={kulesi} alt="kizkulesi" />
-      <h1 className="my-10 text-5xl font-bold text-orange-900 border-amber-900 border-b-4 pb-5">About</h1>
+    <div className=" flex flex-col justify-center items-center">
+      <div>
+        <img src={kulesi} alt="kizkulesi" />
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="my-10 text-5xl font-bold text-orange-900 border-amber-900 border-b-4 pb-5">
+          About
+        </h1>
+        <h3 className="text-3xl text-center m-5 text-orange-900 font-bold">
+          Welcome to ICONIC ISTANBUL - Your Ultimate Guide to Unveiling the
+          City's Hidden Treasures!
+        </h3>
+        <div>
+          {AboutData.map((data) => (
+            <div key={data.id} className="my-8">
+              <h1 className="font-bold text-xl text-orange-900 m-5 md:mx-20">
+                {data.title}
+              </h1>
+              <p className="text-xl  leading-[2rem] m-5 md:mx-20">
+                {data.details}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/AboutContent/AboutData.js
@@ -0,0 +1,38 @@
+export const AboutData = [
+  {
+    id: "1",
+    title: "About Us:",
+    details:
+      "Welcome to ICONIC ISTANBUL website dedicated to sharing the wonders of Istanbul! We are passionate about exploring this wonderful city. Our platform is a community-driven space where locals and visitors alike can come together to share their favorite places, whether it's a historic landmark, a breathtaking viewpoint, a serene walking spot, or a trendy shopping mall.",
+  },
+  {
+    id: "2",
+    title: "Our Mission:",
+    details:
+      "At Iconic Istanbul, our mission is to create a virtual space that celebrates the beauty and diversity of Istanbul. We believe that sharing personal experiences can enrich the journeys of fellow travelers and inspire new adventures. We encourage everyone to be a part of our community, offering their unique insights and capturing the essence of this magnificent city through their lens.",
+  },
+  {
+    id: "3",
+    title: "Share Places Locations On a Map:",
+    details:
+      "Our interactive map feature empowers you to play a vital role in the exploration process. By pinning the exact location of your favorite places, you make it easier for others to discover these enchanting spots. Add important details related to each place, such as historical details, creating a comprehensive guide for fellow adventurers.",
+  },
+  {
+    id: "4",
+    title: "Unleash Your Inner Photographer:",
+    details:
+      "Whether you're a professional shutterbug or a smartphone enthusiast, we welcome all photography enthusiasts to unleash their creativity. Snap stunning images of Istanbul's iconic landmarks, the charming streets, the bustling bazaars, and the breathtaking vistas. Your photographs will not only inspire others but will also etch your name into the ever-growing tapestry of Istanbul's explorers.",
+  },
+  {
+    id: "5",
+    title: "No Login Required:",
+    details:
+      "We want everyone to access and enjoy the valuable information available on our platform without any barriers. That's why we've designed our website to be user-friendly and accessible to all. You can freely browse through the collection of shared places, check the location of the places on the map, and marvel at stunning pictures, all without the need to log in or create an account.",
+  },
+  {
+    id: "6",
+    title: "Let Us Start:",
+    details:
+      "Join us in celebrating Istanbul's charm and allure. Together, let's create a treasure trove of experiences that inspire and unite all who have the pleasure of visiting this incredible city. Happy sharing!",
+  },
+];
